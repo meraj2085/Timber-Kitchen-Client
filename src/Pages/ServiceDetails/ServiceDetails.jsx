e34b13a2c1bacd7961d652c512ca8f459a110ec2
meraj2085 <samirmeraj60@gmail.com>
2022-11-08 18:18:56 +0600

    adding customer reviews

--- src/Pages/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/ServiceDetails/ServiceDetails.jsx
@@ -8,21 +8,21 @@ const ServiceDetails = () => {
 
   return (
     <div>
-      <div class=" bg-slate-50">
+      <div className=" bg-slate-50">
         <div className="pt-5 pb-14">
-          <div class="relative max-w-md mx-auto xl:max-w-2xl min-w-0 break-words bg-white w-full  shadow-lg rounded-xl mt-16">
-            <div class="card">
-              <div class="card-header mx-4 -mt-6">
+          <div className="relative max-w-md mx-auto xl:max-w-2xl min-w-0 break-words bg-white w-full  shadow-lg rounded-xl mt-16">
+            <div className="card">
+              <div className="card-header mx-4 -mt-6">
                 <img
-                  class="w-auto rounded-lg h-[100px] shadow-md"
+                  className="w-auto rounded-lg h-[100px] shadow-md"
                   src={img}
                   alt="service img"
                 />
               </div>
-              <div class="card-body">
-                <h4 class="font-semibold text-2xl">{name}</h4>
-                <p class="mb-4">{details}</p>
-                <p class="font-semibold">Price: ${price}</p>
+              <div className="card-body">
+                <h4 className="font-semibold text-2xl">{name}</h4>
+                <p className="mb-4">{details}</p>
+                <p className="font-semibold">Price: ${price}</p>
               </div>
             </div>
           </div>
