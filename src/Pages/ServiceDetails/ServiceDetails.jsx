a885444c188dc50f427d6f266dc88da45a1c4d0b
meraj2085 <samirmeraj60@gmail.com>
2022-11-08 12:32:23 +0600

    adding service details pafe

--- src/Pages/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/ServiceDetails/ServiceDetails.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const ServiceDetails = () => {
-     const service = useLoaderData()
-     console.log(service);
-     return (
-          <div>
-               <h1>I am details</h1>
+  const service = useLoaderData();
+  const { img, name, details, _id, price } = service;
+
+  return (
+    <div>
+      <div class="min-h-screen bg-slate-50 pb-10">
+        <div className="pt-5">
+          <div class="relative max-w-md mx-auto xl:max-w-2xl min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16">
+            <div class="card">
+              <div class="card-header mx-4 -mt-6">
+                <img
+                  class="w-auto rounded-lg h-[100px] shadow-md"
+                  src={img}
+                  alt="service img"
+                />
+              </div>
+              <div class="card-body">
+                <h4 class="font-semibold text-2xl">{name}</h4>
+                <p class="mb-4">{details}</p>
+                <p class="font-semibold">Price: ${price}</p>
+              </div>
+            </div>
           </div>
-     );
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetails;
