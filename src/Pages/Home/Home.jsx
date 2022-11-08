7b3f4cfb0ddb52f209e5acd52ac80ad2ae1ea7b3
meraj2085 <samirmeraj60@gmail.com>
2022-11-08 08:28:35 +0600

    adding logo

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 const Home = () => {
      return (
-          <div>
-               <h1>I am home</h1>
+          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="flex flex-col items-center justify-between w-full mb-10 lg:flex-row">
+        <div className="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
+          <div className="max-w-xl mb-6">
+            <h2 className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none max-w-lg mb-6">
+              Everything you
+              <br className="hidden md:block" />
+              can imagine{' '}
+              <span className="inline-block text-deep-purple-accent-400">is real</span>
+            </h2>
+            <p className="text-gray-700 text-base md:text-lg">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.</p>
           </div>
+        </div>
+        <div className="flex items-center justify-center lg:w-1/2">
+          <div className="w-2/5">
+            <img className="object-cover" src="https://kitwind.io/assets/kometa/one-girl-phone.png" alt="" />
+          </div>
+          <div className="w-5/12 -ml-16 lg:-ml-32">
+            <img className="object-cover" src="https://kitwind.io/assets/kometa/two-girls-phone.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
      );
 };
 
