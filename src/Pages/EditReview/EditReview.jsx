de18ce1085ceb6fc6cfa41df506a94272346964c
meraj2085 <samirmeraj60@gmail.com>
2022-11-08 21:26:29 +0600

    adding edit

--- src/Pages/EditReview/EditReview.jsx
+++ src/Pages/EditReview/EditReview.jsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const EditReview = () => {
   const review = useLoaderData();
-  console.log(review);
+  const navigate = useNavigate()
 
   const handleReview = (event) => {
     event.preventDefault();
     const form = event.target;
     const message = form.review.value;
-    console.log(message);
+
+    fetch(`http://localhost:5000/review/${review?._id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ message: message }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.modifiedCount > 0) {
+          navigate('/myReviews')
+        }
+      });
   };
 
   return (
@@ -18,6 +31,7 @@ const EditReview = () => {
         <textarea
           name="review"
           id="message"
+          defaultValue={review?.message}
           rows="4"
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Your message..."
