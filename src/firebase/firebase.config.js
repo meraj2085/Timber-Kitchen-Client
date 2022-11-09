4ede0d3a18cf3628e25711dbccf2ad2717a271da
meraj2085 <samirmeraj60@gmail.com>
2022-11-09 12:16:48 +0600

    adding env variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,18 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAi4IPUHl5MxaS8K8dJEYNCrIwTa8j4aMQ",
-  authDomain: "timber-kitchen.firebaseapp.com",
-  projectId: "timber-kitchen",
-  storageBucket: "timber-kitchen.appspot.com",
-  messagingSenderId: "366977369929",
-  appId: "1:366977369929:web:d3f2bf41a775f5f2e17c98"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
