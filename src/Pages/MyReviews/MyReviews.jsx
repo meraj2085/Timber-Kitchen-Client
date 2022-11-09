536923e2e549f78ddbbd1fde7fb0ca765639840e
meraj2085 <samirmeraj60@gmail.com>
2022-11-09 19:14:10 +0600

    adding loader

--- src/Pages/MyReviews/MyReviews.jsx
+++ src/Pages/MyReviews/MyReviews.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Contexts/UserContext";
 import useTitle from "../../hooks/useTitle";
+import Spinner from "../Shared/Spinner/Spinner";
 import MyReviewCard from "./MyReviewCard";
 
 const MyReviews = () => {
   useTitle("My reviews");
   const { user, userSignOut } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
   const [reviews, setReview] = useState([]);
   console.log(reviews);
 
@@ -21,26 +23,36 @@ const MyReviews = () => {
         }
         return res.json();
       })
-      .then((data) => setReview(data));
+      .then((data) => {
+        setLoading(false)
+        setReview(data);
+      });
   }, [user]);
 
   return (
     <div>
+      {loading && <Spinner></Spinner>}
       <div className="text-center text-3xl mt-10">
         <h1>
           My <span className="text-orange-600">reviews</span>
         </h1>
       </div>
-      {!reviews.length == 0 ? <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:mx-20 mx-10 my-20">
-        {reviews.map((review) => (
-          <MyReviewCard
-            key={review._id}
-            reviews={reviews}
-            setReview={setReview}
-            review={review}
-          ></MyReviewCard>
-        ))}
-      </div> : <div className="h-screen flex justify-center pt-56"><p className="text-3xl">No reviews were added</p></div>}
+      {!reviews.length == 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:mx-20 mx-10 my-20">
+          {reviews.map((review) => (
+            <MyReviewCard
+              key={review._id}
+              reviews={reviews}
+              setReview={setReview}
+              review={review}
+            ></MyReviewCard>
+          ))}
+        </div>
+      ) : (
+        <div className="h-screen flex justify-center pt-56">
+          <p className="text-3xl">No reviews were added</p>
+        </div>
+      )}
     </div>
   );
 };
