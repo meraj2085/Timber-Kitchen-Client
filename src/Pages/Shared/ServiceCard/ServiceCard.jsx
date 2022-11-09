b10f1f07c501d425824f22ca67a59c5d6d19556f
meraj2085 <samirmeraj60@gmail.com>
2022-11-09 08:08:53 +0600

    adding ...

--- src/Pages/Shared/ServiceCard/ServiceCard.jsx
+++ src/Pages/Shared/ServiceCard/ServiceCard.jsx
@@ -10,17 +10,23 @@ const ServiceCard = ({ service }) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        <p>{details}</p>
+        <p>
+          {details.length > 100 ? (
+            <>{details.slice(0, 100) + "..."}</>
+          ) : (
+            <>{details}</>
+          )}
+        </p>
         <p>Price: ${price}</p>
         <div className="card-actions justify-end">
           <div className="inline-block mr-2 mt-2">
             <Link to={`/services/${_id}`}>
-            <button
-              type="button"
-              className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-gray-700 hover:bg-gray-900 hover:shadow-lg"
-            >
-              View Details
-            </button>
+              <button
+                type="button"
+                className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-gray-700 hover:bg-gray-900 hover:shadow-lg"
+              >
+                View Details
+              </button>
             </Link>
           </div>
         </div>
