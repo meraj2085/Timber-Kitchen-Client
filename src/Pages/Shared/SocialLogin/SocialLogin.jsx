cc6c1387127da6b32193c7ac9718952c7f7cafbe
meraj2085 <samirmeraj60@gmail.com>
2022-11-09 09:31:49 +0600

    adding navigare

--- src/Pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/Pages/Shared/SocialLogin/SocialLogin.jsx
@@ -1,16 +1,23 @@
 import React, { useContext } from "react";
 import toast from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../Contexts/UserContext";
 import { token } from "../../../utilities/token";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+
   const { googleSignIn } = useContext(AuthContext);
   const handleUserGoogleSignIn = () => {
     googleSignIn()
       .then((result) => {
         const user = result.user;
-        token(user)
-        toast.success('SignIn Successful', {duration: 2000})
+        token(user);
+        navigate(from, { replace: true });
+        toast.success("SignIn Successful", { duration: 2000 });
       })
       .catch((err) => console.error(err.message));
   };
