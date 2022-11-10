8a0b8ff7ae2db56f71b1af8da15ec5689c5356e5
meraj2085 <samirmeraj60@gmail.com>
2022-11-10 06:05:04 +0600

    adding conditional spinner

--- src/Pages/MyReviews/MyReviews.jsx
+++ src/Pages/MyReviews/MyReviews.jsx
@@ -33,27 +33,32 @@ const MyReviews = () => {
 
   return (
     <div>
-      {loading && <Spinner></Spinner>}
-      <div className="text-center text-3xl mt-10">
-        <h1>
-          My <span className="text-orange-600">reviews</span>
-        </h1>
-      </div>
-      {!reviews.length == 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:mx-20 mx-10 my-20">
-          {reviews.map((review) => (
-            <MyReviewCard
-              key={review._id}
-              reviews={reviews}
-              setReview={setReview}
-              review={review}
-            ></MyReviewCard>
-          ))}
-        </div>
+      {loading ? (
+        <Spinner></Spinner>
       ) : (
-        <div className="h-screen flex justify-center pt-56">
-          <p className="text-3xl">No reviews were added</p>
-        </div>
+        <>
+          <div className="text-center text-3xl mt-10">
+            <h1>
+              My <span className="text-orange-600">reviews</span>
+            </h1>
+          </div>
+          {!reviews.length == 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:mx-20 mx-10 my-20">
+              {reviews.map((review) => (
+                <MyReviewCard
+                  key={review._id}
+                  reviews={reviews}
+                  setReview={setReview}
+                  review={review}
+                ></MyReviewCard>
+              ))}
+            </div>
+          ) : (
+            <div className="h-screen flex justify-center pt-56">
+              <p className="text-3xl">No reviews were added</p>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
