167403809e88af749ec23ff8b687eed8d1cc407f
meraj2085 <samirmeraj60@gmail.com>
2022-11-10 09:13:48 +0600

    fixing spinner

--- src/Pages/MyReviews/MyReviews.jsx
+++ src/Pages/MyReviews/MyReviews.jsx
@@ -33,32 +33,27 @@ const MyReviews = () => {
 
   return (
     <div>
-      {loading ? (
-        <Spinner></Spinner>
+      {loading && <Spinner></Spinner>}
+      <div className="text-center text-3xl mt-10">
+        <h1>
+          My <span className="text-orange-600">reviews</span>
+        </h1>
+      </div>
+      {!reviews.length == 0 ? (
+        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 lg:mx-20 mx-10 my-20">
+          {reviews.map((review) => (
+            <MyReviewCard
+              key={review._id}
+              reviews={reviews}
+              setReview={setReview}
+              review={review}
+            ></MyReviewCard>
+          ))}
+        </div>
       ) : (
-        <>
-          <div className="text-center text-3xl mt-10">
-            <h1>
-              My <span className="text-orange-600">reviews</span>
-            </h1>
-          </div>
-          {!reviews.length == 0 ? (
-            <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 lg:mx-20 mx-10 my-20">
-              {reviews.map((review) => (
-                <MyReviewCard
-                  key={review._id}
-                  reviews={reviews}
-                  setReview={setReview}
-                  review={review}
-                ></MyReviewCard>
-              ))}
-            </div>
-          ) : (
-            <div className="h-screen flex justify-center pt-56">
-              <p className="text-3xl">No reviews were added</p>
-            </div>
-          )}
-        </>
+        <div className="h-screen flex justify-center pt-56">
+          <p className="text-3xl">No reviews were added</p>
+        </div>
       )}
     </div>
   );
